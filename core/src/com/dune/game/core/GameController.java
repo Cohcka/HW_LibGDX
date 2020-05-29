4b781153ccd2ecde4413e41a10adf5cfcdda017c
51WORK <m.bufetchikov@gmail.com>
2020-05-29 11:41:30 +0300

    ReadyHW

--- core/src/com/dune/game/core/GameController.java
+++ core/src/com/dune/game/core/GameController.java
@@ -1,12 +1,14 @@
 package com.dune.game.core;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameController {
     private BattleMap map;
     private ProjectilesController projectilesController;
     private TanksController tanksController;
+    private Vector2 CO;
 
     public TanksController getTanksController() {
         return tanksController;
@@ -28,9 +30,11 @@ public class GameController {
         this.tanksController = new TanksController(this);
         this.tanksController.setup(200, 200, Tank.Owner.PLAYER);
         this.tanksController.setup(400, 400, Tank.Owner.PLAYER);
+        CO = new Vector2();
     }
 
     public void update(float dt) {
+        switchSelected();
         tanksController.update(dt);
         projectilesController.update(dt);
         map.update(dt);
@@ -38,5 +42,31 @@ public class GameController {
     }
 
     public void checkCollisions(float dt) {
+        for (int i = tanksController.activeList.size()-1; i >= 0; i--) {
+            for (int j = tanksController.activeList.size()-1; j >= 0; j--) {
+                if(i != j){
+                    if(checkCoordinates(tanksController.activeList.get(i).getPosition(), tanksController.activeList.get(j).getPosition(),80)){
+                        tanksController.activeList.get(j).blockMove();
+                    }
+                }
+            }
+        }
+    }
+
+    public void switchSelected(){
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+            CO.set(Gdx.input.getX(), 720 - Gdx.input.getY());
+            for (int i = tanksController.activeList.size()-1; i >= 0; i--) {
+                if(checkCoordinates(CO,tanksController.activeList.get(i).getPosition(),35)){
+                    tanksController.activeList.get(i).setSelected();
+                }else{
+                    tanksController.activeList.get(i).setNotSelected();
+                }
+            }
+        }
+    }
+
+    protected boolean checkCoordinates (Vector2 obj1, Vector2 obj2, int distance){
+        return(Math.abs(obj1.x-obj2.x) < distance && Math.abs(obj1.y-obj2.y) < distance);
     }
 }
