a8b58d21539a30b623288f4eaa6d33e655d9416c
MaxCohcka <Cohcka@gmail.com>
2020-05-31 22:40:16 +0300

    ReadyHW

--- core/src/com/dune/game/core/Projectile.java
+++ core/src/com/dune/game/core/Projectile.java
@@ -25,7 +25,7 @@ public class Projectile extends GameObject implements Poolable {
     public Projectile(GameController gc) {
         super(gc);
         this.velocity = new Vector2();
-        this.speed = 320.0f;
+        this.speed = 640.0f;
     }
 
     public void setup(Vector2 startPosition, float angle, TextureRegion texture) {
@@ -42,8 +42,5 @@ public class Projectile extends GameObject implements Poolable {
 
     public void update(float dt) {
         position.mulAdd(velocity, dt);
-        if (position.x < 0 || position.x > 1280 || position.y < 0 || position.y > 720) {
-            deactivate();
-        }
     }
 }
--- core/src/com/dune/game/core/ProjectilesController.java
+++ core/src/com/dune/game/core/ProjectilesController.java
@@ -1,13 +1,13 @@
 package com.dune.game.core;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class ProjectilesController extends ObjectPool<Projectile> {
     private GameController gc;
     private TextureRegion projectileTexture;
+    private Vector2 tmp;
 
     @Override
     protected Projectile newObject() {
@@ -17,6 +17,7 @@ public class ProjectilesController extends ObjectPool<Projectile> {
     public ProjectilesController(GameController gc) {
         this.gc = gc;
         this.projectileTexture = Assets.getInstance().getAtlas().findRegion("bullet");
+        this.tmp = new Vector2();
     }
 
     public void render(SpriteBatch batch) {
@@ -26,13 +27,17 @@ public class ProjectilesController extends ObjectPool<Projectile> {
     }
 
     public void setup(Vector2 srcPosition, float angle) {
-        Projectile p = getActiveElement();
+        Projectile p = activateObject();
         p.setup(srcPosition, angle, projectileTexture);
     }
 
     public void update(float dt) {
         for (int i = 0; i < activeList.size(); i++) {
             activeList.get(i).update(dt);
+            tmp.set(activeList.get(i).getPosition());
+            if (tmp.x < 0 || tmp.x > 1280 || tmp.y < 0 || tmp.y > 720 || gc.checkObjectByCO(tmp, 8, 20)) {
+                activeList.get(i).deactivate();
+            }
         }
         checkPool();
     }
